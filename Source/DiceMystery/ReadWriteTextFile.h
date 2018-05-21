cbcd81206ac02e0b53363b6f43aa522ef7954f84
Spidior <seba.gadzinski@hotmail.com>
2018-05-21 22:30:53 +0200

    0.1.7
    Aded Test level

---
+++ Source/DiceMystery/ReadWriteTextFile.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "ReadWriteTextFile.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DICEMYSTERY_API UReadWriteTextFile : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
